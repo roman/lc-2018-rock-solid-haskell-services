19d148cd88a12a41127240dd032ce2942dc26f8a
Roman Gonzalez <open-source@roman-gonzalez.info>
2018-06-03 00:08:03 +0000

    Add config and actions to web-crawl project

--- 5-web-crawl/app/Main.hs
+++ 5-web-crawl/app/Main.hs
@@ -8,9 +8,25 @@ import RIO
 import App.Component (appComponent)
 import Control.Monad.Component (runComponentM)
 
+import Types
+
+run :: RIO App ()
+run = do
+  logInfo "In Run module"
+  forever $ do
+    withRemoteQueue "queue1" $ \queue -> do
+      messages <- liftIO $ receiveMessages queue
+      if null messages then
+        logInfo "No messages found"
+      else do
+        mapM_ (logInfo . display) messages
+        mapM_ (liftIO . deleteRemoteMsg) messages
+    -- delay for 30 seconds
+    threadDelay 5000100
+
 main :: IO ()
 main =
   runComponentM
     "stage-zero"
     appComponent
-    (\app -> runRIO app (return ()))
+    (\app -> runRIO app run)
